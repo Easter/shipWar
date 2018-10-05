d9db1c05fb928a3abc64f3445d0c72cda8256b57
Easter <tyuanzweit@gmail.com>
2018-10-05 12:51:30 +0800

    ship's attributes

---
+++ ship.py
@@ -0,0 +1,33 @@
+#coding=utf-8
+import pygame
+
+class Ship():
+	def __init__(self,ai_settings,screen):
+		#加载飞船图像并获取其外界矩形
+		self.screen = screen
+		self.image = pygame.image.load('images/ship.jpg')
+		self.image = pygame.transform.scale(self.image,(25,28))
+		self.rect = self.image.get_rect()
+		self.screen_rect = screen.get_rect()
+		self.ai_settings = ai_settings
+
+		#将每艘飞船放在屏幕底部中央
+		self.rect.centerx = self.screen_rect.centerx #飞船中心的X坐标
+		self.rect.bottom = self.screen_rect.bottom #飞船下边缘的Y坐标
+		self.center = float(self.rect.centerx)
+		# 移动标志
+		self.moving_right = False
+		self.moving_left = False
+
+	def update(self):
+		#根据移动标志调整飞船的位置
+		#更新飞船的center值而不是rect
+		if self.moving_right and self.rect.right < self.screen_rect.right:
+			self.center += self.ai_settings.ship_speed_factor
+		if self.moving_left and self.rect.left > 0:
+			self.center -= self.ai_settings.ship_speed_factor
+		#根据self.center 更新self.rect.centerx 的值
+		self.rect.centerx = self.center
+	def blitme(self):
+		#在指定位置绘制飞船
+		self.screen.blit(self.image,self.rect)
